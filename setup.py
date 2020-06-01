fc8ed181af68d9b59468b3a09043c0531ddaba71
n.kuznetsov <n.kuznetsov@a1qa.com>
2020-06-01 19:24:30 +0400

    created structure of repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='aquality-selenium-core',
+    version='0.0.1',
+    packages=find_packages(include=['aquality_selenium_core*']),
+    url='https://github.com/aquality-automation/aquality-selenium-core-python',
+    license='Apache 2.0',
+    author='',
+    author_email='',
+    description=''
+)
