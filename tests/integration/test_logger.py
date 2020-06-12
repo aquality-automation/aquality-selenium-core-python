c80a95550602be26dc050d0ed8c1adb5bd7804a3
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-06-12 18:24:16 +0300

    Add tests for logger

---
+++ tests/integration/test_logger.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from aquality_selenium_core.logging.logger import Logger
+
+logger = Logger()
+
+HANDLER_TEST_LOG_FILE_NAME = 'test.log'
+
+
+class TestLogger:
+    file_handler = logging.FileHandler(HANDLER_TEST_LOG_FILE_NAME)
+    file_handler.setLevel(logging.DEBUG)
+
+    def test_should_be_possible_to_add_handler(self):
+        log_message = 'Add handler test message'
+        logger.add_handler(self.file_handler)
+        logger.info(log_message)
+
+        log_data = self._read_file_data(HANDLER_TEST_LOG_FILE_NAME)
+        assert log_message in log_data
+
+    def test_should_be_possible_to_remove_handler(self):
+        log_message = 'Remove handler test message'
+        logger.add_handler(self.file_handler)
+        logger.remove_handler(self.file_handler)
+        logger.info(log_message)
+
+        log_data = self._read_file_data(HANDLER_TEST_LOG_FILE_NAME)
+        assert log_message not in log_data
+
+    @staticmethod
+    def _read_file_data(file_name):
+        with open(file_name, 'r') as file:
+            return file.read()
