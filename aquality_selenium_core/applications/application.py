3715b46eedf90dbdd6f218770926d1b51a6b4169
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-06-22 17:43:37 +0300

    Fix docstring

--- aquality_selenium_core/applications/application.py
+++ aquality_selenium_core/applications/application.py
@@ -1,3 +1,4 @@
+"""Defines abstraction of any application (browser, desktop or mobile driver) controlled by Selenium WebDriver API."""
 from abc import ABC
 from abc import abstractmethod
 from datetime import timedelta
@@ -6,29 +7,24 @@ from selenium.webdriver import Remote
 
 
 class AbstractApplication(ABC):
-    """
-    Interface of any application controlled by Selenium WebDriver API.
-    """
+    """Interface of any application controlled by Selenium WebDriver API."""
 
     @property
     @abstractmethod
     def driver(self) -> Remote:
-        """
-        :return: Current instance of driver.
-        """
+        """:return: Current instance of driver."""
         pass
 
     @abstractmethod
     def is_started(self) -> bool:
-        """
-        :return: Is the application already running or not.
-        """
+        """:return: Is the application already running or not."""
         pass
 
     @abstractmethod
     def set_implicit_wait_timeout(self, value: timedelta) -> None:
         """
-        Sets implicit wait timeout to Selenium WebDriver.
-        :param value: timeout value to set.
+        Set implicit wait timeout to Selenium WebDriver.
+
+        :param value: Timeout value to set.
         """
         pass
