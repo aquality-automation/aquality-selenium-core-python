0262b87f7673846910118e68195c1c79b871d68e
Pavel Anihimovsky <47742067+pavelanihimovsky@users.noreply.github.com>
2020-06-30 11:31:23 +0300

    Feature/7 elements interfaces (#27)
    * Define application interface
    
    * Define application interface
    
    * Change type of implicit timeout to timedelta
    
    * Define Element base class
    
    * Update LICENSE
    
    * Rename file
    
    * Rename files and remove unused imports
    
    * Update requirements
    
    * Rename abstract class
    
    * Fix issues found by linter
    
    * Fix docstring
    
    * Fix linter issues
    
    * Fix linter issues
    
    * Fix linter issues
    
    * Remove requirements file
    
    * Remove logger
    
    * Remove logger
    
    * Implement some methods from element
    
    * Define Parent interface and ElementFactory (#12)
    
    * Define Parent interface and ElementFactory
    
    * Add find_child_elements method to Parent and Element
    
    * Update to base branch
    
    * Fix linter issues
    
    * Fix linter issues
    
    * Fix typos and linter issues

---
+++ aquality_selenium_core/configurations/element_cache_configuration.py
@@ -0,0 +1,8 @@
+"""Module defines abstraction for element cache configuration."""
+from abc import ABC
+
+
+class AbstractElementCacheConfiguration(ABC):
+    """Provides element's cache configuration."""
+
+    pass
