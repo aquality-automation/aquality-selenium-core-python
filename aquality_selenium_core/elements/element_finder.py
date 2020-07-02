7433993c0dca32718d232bf9f92b0c2cf27962f3
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-07-02 18:13:18 +0300

    Define interface for element finder

--- aquality_selenium_core/elements/element_finder.py
+++ aquality_selenium_core/elements/element_finder.py
@@ -1,5 +1,13 @@
 """Module defines abstraction for element finder."""
 from abc import ABC
+from datetime import timedelta
+from typing import Callable
+from typing import List
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webelement import WebElement
+
+from aquality_selenium_core.elements.element_state import ElementState
 
 
 class AbstractElementFinder(ABC):
@@ -13,4 +21,39 @@ class AbstractElementFinder(ABC):
     - with - Predicate.
     """
 
-    pass
+    def find_element(
+        self,
+        locator: By,
+        element_state_condition: Callable[[WebElement], bool],
+        state: ElementState = ElementState.EXISTS_IN_ANY_STATE,
+        timeout: timedelta = timedelta.min,
+    ) -> WebElement:
+        """
+        Find element in desired ElementState or state defined by predicate.
+
+        :param locator: element locator.
+        :param element_state_condition: predicate to define element state.
+        :param state: desired ElementState.
+        :param timeout: timeout for search.
+        :return: Found element.
+        :raises: NoSuchElementException if element was not found in time in desired state.
+        """
+        pass
+
+    def find_elements(
+        self,
+        locator: By,
+        element_state_condition: Callable[[WebElement], bool],
+        state: ElementState = ElementState.EXISTS_IN_ANY_STATE,
+        timeout: timedelta = timedelta.min,
+    ) -> List[WebElement]:
+        """
+        Find elements in desired ElementState or state defined by predicate.
+
+        :param locator: element locator.
+        :param element_state_condition: predicate to define element state.
+        :param state: desired ElementState.
+        :param timeout: timeout for search.
+        :return: List of found elements.
+        """
+        pass
