4fa86bfaa4c96e14fcc34303a6087ee7ba88d012
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-07-07 14:30:43 +0300

    Define implementation of element finder

--- aquality_selenium_core/elements/element_finder.py
+++ aquality_selenium_core/elements/element_finder.py
@@ -9,6 +9,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 from aquality_selenium_core.elements.element_state import ExistsInAnyState
+from aquality_selenium_core.localization.localized_logger import AbstractLocalizedLogger
+from aquality_selenium_core.waitings.conditional_wait import AbstractConditionalWait
 
 
 class AbstractElementFinder(ABC):
@@ -33,6 +35,50 @@ class AbstractElementFinder(ABC):
         pass
 
     @abstractmethod
+    def find_elements(
+        self,
+        locator: By,
+        desired_state: Callable = ExistsInAnyState(),
+        timeout: timedelta = timedelta.min,
+    ) -> List[WebElement]:
+        """
+        Find elements in desired state defined by callable object.
+
+        :param locator: element locator.
+        :param desired_state: desired element state as callable object.
+        :param timeout: timeout for search.
+        :return: List of found elements.
+        """
+        pass
+
+
+class ElementFinder(AbstractElementFinder):
+    """Implementation of AbstractElementFinder."""
+
+    def __init__(
+        self, logger: AbstractLocalizedLogger, conditional_wait: AbstractConditionalWait
+    ):
+        """Initialize finder with required dependencies."""
+        self.__logger = logger
+        self.__conditional_wait = conditional_wait
+
+    def find_element(
+        self,
+        locator: By,
+        desired_state: Callable = ExistsInAnyState(),
+        timeout: timedelta = timedelta.min,
+    ) -> WebElement:
+        """
+        Find element in desired state defined by callable object.
+
+        :param locator: element locator.
+        :param desired_state: desired element state as callable object.
+        :param timeout: timeout for search.
+        :return: Found element.
+        :raises: NoSuchElementException if element was not found in time in desired state.
+        """
+        pass
+
     def find_elements(
         self,
         locator: By,
