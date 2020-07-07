fc40fccfa143d3b56be88ff38f05b56169fc9a5a
Pavel Anihimovsky <pavel.anihimovsky@gmail.com>
2020-07-07 12:58:52 +0300

    Extend element interface

--- aquality_selenium_core/elements/element.py
+++ aquality_selenium_core/elements/element.py
@@ -1,16 +1,23 @@
 """Abstraction for any custom element of the web, desktop of mobile application."""
+import logging
 from abc import ABC
 from abc import abstractmethod
 from datetime import timedelta
 from typing import Callable
+from typing import cast
 from typing import List
 
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 
 from aquality_selenium_core.applications.application import AbstractApplication
 from aquality_selenium_core.configurations.element_cache_configuration import (
     AbstractElementCacheConfiguration,
+)
+from aquality_selenium_core.configurations.logger_configuration import (
+    AbstractLoggerConfiguration,
 )
 from aquality_selenium_core.elements.element_cache_handler import (
     AbstractElementCacheHandler,
@@ -23,6 +30,9 @@ from aquality_selenium_core.elements.element_state_provider import (
 )
 from aquality_selenium_core.elements.elements_count import ElementsCount
 from aquality_selenium_core.elements.parent import TElement
+from aquality_selenium_core.localization.localization_manager import (
+    AbstractLocalizationManager,
+)
 from aquality_selenium_core.localization.localized_logger import AbstractLocalizedLogger
 from aquality_selenium_core.utilities.action_retrier import TReturn
 from aquality_selenium_core.utilities.element_action_retrier import (
@@ -38,7 +48,8 @@ class AbstractElement(ABC):
         """Initialize element with locator, name and state."""
         self.__locator = locator
         self.__name = name
-        self.__state = state
+        self.__element_state = state
+        self.__element_cache_handler = cast(AbstractElementCacheHandler, None)
 
     @property
     def locator(self) -> By:
@@ -57,6 +68,10 @@ class AbstractElement(ABC):
         :return: Element name.
         """
         return self.__name
+
+    @property
+    def _element_state(self):
+        return self.__element_state
 
     @property
     def state(self) -> AbstractElementStateProvider:
@@ -121,14 +136,27 @@ class AbstractElement(ABC):
         Get current element by specified locator.
 
         Default timeout is provided in TimeoutConfiguration.
-        Throws NoSuchElementException if element not found.
         :return: Instance of WebElement if found.
+        :raises: NoSuchElementException if element not found.
         """
-        raise NotImplementedError
+        try:
+            if self._cache_configuration.is_enabled:
+                element = self._cache.get_element(timeout)
+            else:
+                element = self._element_finder.find_element(
+                    self.__locator, self.__element_state, timeout
+                )
+            return element
+        except NoSuchElementException:
+            if self._logger_configuration.log_page_source:
+                self.__log_page_source()
+            raise
 
-    @property
-    def _element_state(self):
-        return self.__state
+    def __log_page_source(self) -> None:
+        try:
+            logging.debug(f"Page source:\n{self._application.driver.page_source}")
+        except WebDriverException:
+            logging.error("An exception occurred while tried to save the page source")
 
     @property
     @abstractmethod
@@ -147,7 +175,7 @@ class AbstractElement(ABC):
 
     @property
     @abstractmethod
-    def _element_cache_configuration(self) -> AbstractElementCacheConfiguration:
+    def _cache_configuration(self) -> AbstractElementCacheConfiguration:
         pass
 
     @property
@@ -162,6 +190,11 @@ class AbstractElement(ABC):
 
     @property
     @abstractmethod
+    def _localization_manager(self) -> AbstractLocalizationManager:
+        pass
+
+    @property
+    @abstractmethod
     def _conditional_wait(self) -> AbstractConditionalWait:
         pass
 
@@ -169,6 +202,10 @@ class AbstractElement(ABC):
     @abstractmethod
     def _element_type(self) -> str:
         pass
+
+    @property
+    def _logger_configuration(self) -> AbstractLoggerConfiguration:
+        return self._localized_logger.configuration
 
     @property
     def _cache(self) -> AbstractElementCacheHandler:
