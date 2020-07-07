1f53272d959e733c3a5deab94d54bf3c19739190
Pavel Anihimovsky <47742067+pavelanihimovsky@users.noreply.github.com>
2020-07-07 14:14:21 +0300

    Implement action retrier and element action retrier (#35)
    * Implement logger
    
    * Add ability to add and remove logger handlers
    
    * Add tests for logger
    
    * Fix issues found by linter
    
    * Fix issues found by linter
    
    * Fix issues found by linter
    
    * Fix issues found by linter
    
    * Fix issues found by linter
    
    * Fix issues found by linter
    
    * Update docstring
    
    * Remove python 3.5 from GitHub actions
    
    * Updat setup.cfg and tox.ini
    
    * Update asserts in tests to hamcrest
    
    * Define log method explicitly
    
    * Define localization manager and localized logger
    
    * Rename method
    
    * Rename private fields and add localization files
    
    * Rename private fields
    
    * Implement localized logger
    
    * Remove logger classes
    
    * Update localization files
    
    * Fix localized template strings
    
    * Rename args and kwargs parameters
    
    * Rename args and kwargs parameters
    
    * Rename args and kwargs parameters
    
    * Define missing interfaces
    
    * Use timedelta instead of int for timeouts
    
    * Use timedelta instead of int for timeouts
    
    * Define interface for element finder
    
    * Remove state parameter from element finder
    
    * Add state parameter to element finder
    
    * Implement action retrier
    
    * Feature/25 element action retrier (#36)
    
    * Implement element action retrier
    
    * Implement element action retrier
    
    * Remove resources
    
    * Format test
    
    * Format test

--- aquality_selenium_core/utilities/element_action_retrier.py
+++ aquality_selenium_core/utilities/element_action_retrier.py
@@ -1,10 +1,19 @@
 """Module defines abstraction for retry functionality."""
 from abc import ABC
 from abc import abstractmethod
+from typing import Callable
 from typing import List
 from typing import Type
 
+from selenium.common.exceptions import InvalidElementStateException
+from selenium.common.exceptions import StaleElementReferenceException
+
+from aquality_selenium_core.configurations.retry_configuration import (
+    AbstractRetryConfiguration,
+)
 from aquality_selenium_core.utilities.action_retrier import AbstractActionRetrier
+from aquality_selenium_core.utilities.action_retrier import ActionRetrier
+from aquality_selenium_core.utilities.action_retrier import TReturn
 
 
 class AbstractElementActionRetrier(AbstractActionRetrier, ABC):
@@ -19,3 +28,36 @@ class AbstractElementActionRetrier(AbstractActionRetrier, ABC):
         :return: Supported exceptions.
         """
         pass
+
+
+class ElementActionRetrier(ActionRetrier, AbstractElementActionRetrier):
+    """Retrier for action on elements."""
+
+    def __init__(self, retry_configuration: AbstractRetryConfiguration):
+        """Initialize retrier with configuration."""
+        ActionRetrier.__init__(self, retry_configuration)
+
+    def do_with_retry(
+        self,
+        function: Callable[..., TReturn],
+        handled_exceptions: List[Type[Exception]] = [],
+    ) -> TReturn:
+        """
+        Retry the action when the handled exception occurred.
+
+        :param function: Action to be applied.
+        :param handled_exceptions: Exceptions to be handled.
+        :return: Result of the function.
+        """
+        exceptions_to_handle = (
+            handled_exceptions if handled_exceptions else self.get_handled_exceptions()
+        )
+        return super().do_with_retry(function, exceptions_to_handle)
+
+    def get_handled_exceptions(self) -> List[Type[Exception]]:
+        """
+        Return supported exceptions.
+
+        :return: Supported exceptions.
+        """
+        return [StaleElementReferenceException, InvalidElementStateException]
---
+++ tests/test_utilities/test_action_retrier.py
@@ -0,0 +1,111 @@
+from datetime import timedelta
+
+import pytest
+from hamcrest import assert_that
+from hamcrest import calling
+from hamcrest import equal_to
+from hamcrest import is_not
+from hamcrest import raises
+from selenium.common.exceptions import InvalidElementStateException
+from selenium.common.exceptions import StaleElementReferenceException
+
+from aquality_selenium_core.configurations.retry_configuration import (
+    AbstractRetryConfiguration,
+)
+from aquality_selenium_core.utilities.action_retrier import AbstractActionRetrier
+from aquality_selenium_core.utilities.action_retrier import ActionRetrier
+from aquality_selenium_core.utilities.element_action_retrier import (
+    AbstractElementActionRetrier,
+)
+from aquality_selenium_core.utilities.element_action_retrier import ElementActionRetrier
+
+
+class TestActionRetries:
+    def test__do_with_retry__should_raise_error_if_there_are_no_handled_exceptions(
+        self,
+    ):
+        def func():
+            raise CustomException()
+
+        assert_that(
+            calling(self.__get_action_retrier().do_with_retry).with_args(func),
+            raises(CustomException),
+            "Custom exception is not raised",
+        )
+
+    def test__do_with_retry__should_be_possible_to_ignore_custom_exception(self):
+        raise_exception = {"value": True}
+
+        def func():
+            if raise_exception["value"]:
+                raise_exception["value"] = False
+                raise CustomException()
+            return True
+
+        assert_that(
+            self.__get_action_retrier().do_with_retry(
+                func, handled_exceptions=[CustomException]
+            ),
+            equal_to(True),
+            "Exception is raised",
+        )
+
+    def test__do_with_retry__should_not_raise_exception_when_it_is_not_raised(self):
+        def func():
+            return False
+
+        assert_that(
+            self.__get_action_retrier().do_with_retry(func),
+            equal_to(False),
+            "Action is not completed",
+        )
+
+    @staticmethod
+    def __get_action_retrier() -> AbstractActionRetrier:
+        retry_configuration = CustomRetryConfiguration()
+        return ActionRetrier(retry_configuration)
+
+
+class TestElementActionRetrier:
+    @pytest.mark.parametrize(
+        "exception_type,exception",
+        [
+            (StaleElementReferenceException, StaleElementReferenceException()),
+            (InvalidElementStateException, InvalidElementStateException()),
+        ],
+    )
+    def test__do_with_retry__should_not_raise_exceptions_handled_by_default(
+        self, exception_type, exception
+    ):
+        raise_exception = {"value": True}
+
+        def func():
+            if raise_exception["value"]:
+                raise_exception["value"] = False
+                raise exception
+            return True
+
+        assert_that(
+            calling(self.__get_element_action_retrier().do_with_retry).with_args(func),
+            is_not(raises(exception_type)),
+            "Default exception is not ignored",
+        )
+
+    @staticmethod
+    def __get_element_action_retrier() -> AbstractElementActionRetrier:
+        retry_configuration = CustomRetryConfiguration()
+        return ElementActionRetrier(retry_configuration)
+
+
+class CustomException(Exception):
+    pass
+
+
+class CustomRetryConfiguration(AbstractRetryConfiguration):
+    @property
+    def number(self) -> int:
+        return 2
+
+    @property
+    def polling_interval(self) -> timedelta:
+        return timedelta(milliseconds=500)
