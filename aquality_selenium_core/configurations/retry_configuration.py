f8df647cc59cf58acce8ea03ddc45620a8d393e4
Pavel Anihimovsky <47742067+pavelanihimovsky@users.noreply.github.com>
2020-07-08 19:09:44 +0300

    Feature/4 configurations (#38)
    * Define utilities classes and implementation of work with files
    
    * Define utilities classes and implementation of work with files
    
    * Define configuration classes
    
    * fix review issues
    
    * Merge master
    
    * Implement configurations
    
    * Update configurations
    
    * Fix default value for logger configuration
    
    * Use generic types in settings file
    
    * Refactor configurations
    
    Co-authored-by: ahramovich <a.hramovich@a1qa.com>

--- aquality_selenium_core/configurations/retry_configuration.py
+++ aquality_selenium_core/configurations/retry_configuration.py
@@ -3,6 +3,8 @@ from abc import ABC
 from abc import abstractmethod
 from datetime import timedelta
 
+from aquality_selenium_core.utilities.settings_file import AbstractSettingsFile
+
 
 class AbstractRetryConfiguration(ABC):
     """Describes retry configuration."""
@@ -18,3 +20,22 @@ class AbstractRetryConfiguration(ABC):
     def polling_interval(self) -> timedelta:
         """Get the polling interval used in retry."""
         pass
+
+
+class RetryConfiguration(AbstractRetryConfiguration):
+    """Describes retry configuration."""
+
+    def __init__(self, settings_file: AbstractSettingsFile):
+        """Initialize configuration with settings file."""
+        self.__settings_file = settings_file
+
+    @property
+    def number(self) -> int:
+        """Get the number of attempts to retry."""
+        return self.__settings_file.get_value("retry.number")
+
+    @property
+    def polling_interval(self) -> timedelta:
+        """Get the polling interval used in retry."""
+        config_value: int = self.__settings_file.get_value("retry.pollingInterval")
+        return timedelta(milliseconds=config_value)
--- aquality_selenium_core/configurations/timeout_configuration.py
+++ aquality_selenium_core/configurations/timeout_configuration.py
@@ -3,6 +3,8 @@ from abc import ABC
 from abc import abstractmethod
 from datetime import timedelta
 
+from aquality_selenium_core.utilities.settings_file import AbstractSettingsFile
+
 
 class AbstractTimeoutConfiguration(ABC):
     """Abstraction for timeout configuration."""
@@ -30,3 +32,38 @@ class AbstractTimeoutConfiguration(ABC):
     def command(self) -> timedelta:
         """Get WebDriver Command timeout."""
         pass
+
+
+class TimeoutConfiguration(AbstractTimeoutConfiguration):
+    """Abstraction for timeout configuration."""
+
+    def __init__(self, settings_file: AbstractSettingsFile):
+        """Initialize configuration with settings file."""
+        self.__settings_file = settings_file
+
+    @property
+    def implicit(self) -> timedelta:
+        """Get WedDriver ImplicitWait timeout."""
+        config_value = self.__get_config_value("timeouts.timeoutImplicit")
+        return timedelta(seconds=config_value)
+
+    @property
+    def condition(self) -> timedelta:
+        """Get default ConditionalWait timeout."""
+        config_value = self.__get_config_value("timeouts.timeoutCondition")
+        return timedelta(seconds=config_value)
+
+    @property
+    def polling_interval(self) -> timedelta:
+        """Get ConditionalWait polling interval."""
+        config_value = self.__get_config_value("timeouts.timeoutPollingInterval")
+        return timedelta(milliseconds=config_value)
+
+    @property
+    def command(self) -> timedelta:
+        """Get WebDriver Command timeout."""
+        config_value = self.__get_config_value("timeouts.timeoutCommand")
+        return timedelta(seconds=config_value)
+
+    def __get_config_value(self, key: str) -> int:
+        return self.__settings_file.get_value(key)
